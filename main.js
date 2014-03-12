d60f532b3c409a47624c3b499958b0dc105da08d
Jordan Morano <jordan.morano@gmail.com>
2014-03-12 19:23:25 -0400

    Initial setup with files and raw data

---
+++ main.js
@@ -0,0 +1,7 @@
+
+// JSON data pts
+// {"sighted_at": "19951009", "reported_at": "19951009", "location": " Iowa City, IA", "shape": "", "duration": "", "description": "Man repts. witnessing &quot;flash, followed by a classic UFO, w/ a tailfin at back.&quot; Red color on top half of tailfin. Became triangular."}
+
+window.onload = function () {
+
+};
